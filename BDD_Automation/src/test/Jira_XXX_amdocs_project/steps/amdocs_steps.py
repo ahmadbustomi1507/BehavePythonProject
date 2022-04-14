d7278fc69849f98888dae9168a253733f9d56bb9
tomi <adl.ahmadb@xl.co.id>
2022-04-14 16:52:16 +0700

    -

--- BDD_Automation/src/test/Jira_XXX_amdocs_project/steps/amdocs_steps.py
+++ BDD_Automation/src/test/Jira_XXX_amdocs_project/steps/amdocs_steps.py
@@ -7,17 +7,16 @@
 # res = api.API_SUBSCRIBER_PROFILE_INFO_ENDPOINT(params=params)
 # $ behave -f allure_behave.formatter:AllureFormatter -o %result% ./features
 # $ allure serve %result%
+import  logging
 
 @given('User getting the MSISDN : {MSISDN}')
 def step_impl(context,MSISDN):
-    print('msisdn {}'.format(MSISDN))
-
-
+    logging.info('User getting the MSISDN')
+    # print('msisdn {}'.format(MSISDN))
 
 @when('User Hit the API with the payload')
 def step_impl(context):
     print('when success')
-
 
 @then('User successfully hit the API with status code 200')
 def step_impl(context):
